bd53ffd7574fca890bb5d358bab6057330a4d820
reckdave <recksttacc@gmail.com>
2025-04-19 12:06:52 +0100

    added a wholeeeeeee bunch of shit. including new multiplayer stuff

---
+++ assets/scripts/GameHandler.gd
@@ -0,0 +1,38 @@
+extends Node
+
+@onready var sheep_scene : PackedScene = preload("res://assets/objects/player.tscn")
+var alive_players : Array = []
+var world_node : Node3D
+
+signal game_started
+signal game_end
+
+func _ready() -> void:
+	randomize()
+
+@rpc("authority","call_local","reliable")
+func start_game(world : PackedScene = load("res://assets/scenes/ireland.tscn")):
+	if (multiplayer.get_remote_sender_id() != 1): return
+	if (MultiplayerHandler.players.size() <= 1): OS.alert("NOT ENOUGH PLAYERS..","HEY"); return
+	
+	Steam.setLobbyJoinable(MultiplayerHandler.lobby_id,false)
+	multiplayer.multiplayer_peer.refuse_new_connections = true
+	
+	world_node = world.instantiate()
+	get_tree().root.add_child(world_node)
+	
+	for player in MultiplayerHandler.players:
+		var new_player = sheep_scene.instantiate()
+		new_player.name = str(player)
+		new_player.username = str(MultiplayerHandler.players[player]["username"])
+		new_player.global_position = Vector3(randi_range(-100,100),5,randi_range(-100,100))
+		alive_players.append(player)
+		world_node.add_child(new_player)
+	game_started.emit()
+
+@rpc("authority","call_local","reliable")
+func end_game():
+	await get_tree().create_timer(4).timeout
+	world_node.queue_free()
+	alive_players.clear()
+	game_end.emit()
--- assets/scripts/MultiplayerHandler.gd
+++ assets/scripts/MultiplayerHandler.gd
@@ -3,8 +3,12 @@ extends Node
 const port : int = 8080
 const default_ip : String = "127.0.0.1"
 
+var peer
 var players : Dictionary = {}
 var lobby_id : int = 0
+
+signal updated_player_list
+signal failed_to_connect
 
 func _ready() -> void:
 	multiplayer.peer_connected.connect(_peer_connected)
@@ -12,12 +16,87 @@ func _ready() -> void:
 	multiplayer.connected_to_server.connect(_connected_to_server)
 	multiplayer.connection_failed.connect(_connection_failed)
 
+# multiplayer calls
+func create_server(server_type,visibility):
+	match (server_type):
+		0: # STEAM
+			peer = SteamMultiplayerPeer.new()
+			peer.create_lobby(visibility,50)
+			peer.lobby_created.connect(_on_lobby_created)
+			
+			multiplayer.multiplayer_peer = peer
+		1: #LOCAL
+			peer = ENetMultiplayerPeer.new()
+			var error = peer.create_server(port,50)
+			if error:
+				peer = null
+				return error
+			multiplayer.multiplayer_peer = peer
+			sendplayerdata(Global.player_data,multiplayer.get_unique_id())
+	return true
+func join_server(server_type,ip):
+	match (server_type):
+		0: # STEAM
+			peer = SteamMultiplayerPeer.new()
+			peer.connect_lobby(int(ip))
+			peer.lobby_joined.connect(_on_lobby_joined)
+		1: # LOCAL
+			peer = ENetMultiplayerPeer.new()
+			var error = peer.create_client(ip,port)
+			if error:
+				peer = null
+				return error
+			multiplayer.multiplayer_peer = peer
+	return true
+
+# multiplayer rpcs
+@rpc("any_peer","call_remote","reliable")
+func sendplayerdata(data,pid):
+	if !(players.has(pid)):
+		data["pid"] = pid
+		players[pid] = data
+		#print(players[pid])
+		updated_player_list.emit()
+	
+	if (multiplayer.is_server()):
+		for player in players:
+			sendplayerdata.rpc(players[player],player)
+
 # multiplayer connects
-func _peer_connected():
+func _peer_connected(pid):
 	pass
-func _peer_disconnected():
-	pass
+func _peer_disconnected(pid):
+	players.erase(pid)
+	updated_player_list.emit()
 func _connected_to_server():
-	pass
+	#print("%s has joined: %s" % [multiplayer.get_unique_id(),multiplayer.get_remote_sender_id()])
+	sendplayerdata.rpc_id(1,Global.player_data,multiplayer.get_unique_id())
 func _connection_failed():
-	pass
+	failed_to_connect.emit()
+# STEAM
+func _on_lobby_created(connect: int, this_lobby_id: int) -> void:
+	if connect == 1:
+		# Set the lobby ID
+		lobby_id = this_lobby_id
+		print("Created a lobby: %s" % lobby_id)
+
+		# Set this lobby as joinable, just in case, though this should be done by default
+		Steam.setLobbyJoinable(lobby_id, true)
+
+		# Set some lobby data
+		Steam.setLobbyData(lobby_id, "name", "%s's CATTLE Lobby" % Global.player_data.username)
+		Steam.setLobbyData(lobby_id, "mode", "CATTLE-MULTIPLAYER")
+		
+		DisplayServer.clipboard_set(str(this_lobby_id))
+		OS.alert("THE LOBBY ID IS ON YOUR CLIPBOARD","HEY")
+		
+		sendplayerdata(Global.player_data,multiplayer.get_unique_id())
+		# Allow P2P connections to fallback to being relayed through Steam if needed
+		#var set_relay: bool = Steam.allowP2PPacketRelay(true)
+		#print("Allowing Steam to be relay backup: %s" % set_relay)
+func _on_lobby_joined(this_lobby_id: int, _permissions: int, _locked: bool, response: int) -> void:
+	# If joining was successful
+	if response == Steam.CHAT_ROOM_ENTER_RESPONSE_SUCCESS:
+		# Set this lobby ID as your lobby ID
+		lobby_id = this_lobby_id
+		sendplayerdata.rpc_id(1,Global.player_data,multiplayer.get_unique_id())
